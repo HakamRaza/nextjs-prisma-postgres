4687b4ba6e6e0d3e3e4dfea3981338079418c238
HakamRaza <ismatcd@yahoo.com>
2023-06-12 18:29:53 +0800

    query db

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,25 +2,31 @@ import React from "react"
 import { GetStaticProps } from "next"
 import Layout from "../components/Layout"
 import Post, { PostProps } from "../components/Post"
+import prisma from '../lib/prisma';
 
 export const getStaticProps: GetStaticProps = async () => {
-  const feed = [
-    {
-      id: "1",
-      title: "Prisma is the perfect ORM for Next.js",
-      content: "[Prisma](https://github.com/prisma/prisma) and Next.js go _great_ together!",
-      published: false,
+  const feed = await prisma.post.findMany({
+    // A where filter is specified to include only Post records where published is true
+    where: { published: true },
+    // The name of the author of the Post record is queried as well and will be included in the returned objects
+    // nested select. Select parent and child table together
+    // there issue with Date object given. Need to JSON.parse(JSON.stringify(result)). But below we not include direct
+    select: {
+      id: true,
+      title: true,
+      content: true,
+      published: true,
       author: {
-        name: "Nikolas Burk",
-        email: "burk@prisma.io",
+        select: { name: true },
       },
     },
-  ]
-  return { 
-    props: { feed }, 
-    revalidate: 10 
-  }
-}
+  });
+
+  return {
+    props: { feed },
+    revalidate: 10,
+  };
+};
 
 type Props = {
   feed: PostProps[]

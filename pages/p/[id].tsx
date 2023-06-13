7304b74fb0afdfecab0cc369af67e45fada65dcb
HakamRaza <ismatcd@yahoo.com>
2023-06-13 16:56:23 +0800

    add CRUD posts

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,15 +1,17 @@
-import React from "react"
-import { GetServerSideProps } from "next"
-import ReactMarkdown from "react-markdown"
-import Layout from "../../components/Layout"
-import { PostProps } from "../../components/Post"
+import React from 'react';
+import { GetServerSideProps } from 'next';
+import ReactMarkdown from 'react-markdown';
+import Router from 'next/router';
+import Layout from '../../components/Layout';
+import { PostProps } from '../../components/Post';
+import { useSession } from 'next-auth/react';
 import prisma from '../../lib/prisma';
 
 // dynamic data, use getServerSideProps
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   const post = await prisma.post.findUnique({
     where: {
-      id: Number(params?.id),
+      id: String(params?.id),
     },
     select: {
       id: true,
@@ -17,7 +19,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
       content: true,
       published: true,
       author: {
-        select: { name: true },
+        select: { name: true, email:true },
       },
     },
   });
@@ -27,42 +29,80 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
   };
 };
 
+async function publishPost(id: string): Promise<void> {
+  await fetch(`/api/publish/${id}`, {
+    method: 'PUT',
+  });
+  await Router.push('/');
+
+}
+
+
+async function deletePost(id: string): Promise<void> {
+  await fetch(`/api/post/${id}`, {
+    method: 'DELETE',
+  });
+  Router.push('/');
+}
+
+
 const Post: React.FC<PostProps> = (props) => {
-  let title = props.title
-  if (!props.published) {
-    title = `${title} (Draft)`
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <div>Authenticating ...</div>;
   }
+
+  const userHasValidSession = Boolean(session);
+  const postBelongsToUser = session?.user?.email === props.author?.email;
+  let title = props.title;
+
+  if (!props.published) {
+    title = `${title} (Draft)`;
+  }
+
+  // console.log(userHasValidSession, postBelongsToUser, title, session.user.email, props.author.email)
 
   return (
     <Layout>
       <div>
         <h2>{title}</h2>
-        <p>By {props?.author?.name || "Unknown author"}</p>
+        <p>By {props?.author?.name || 'Unknown author'}</p>
         <ReactMarkdown children={props.content} />
+        {
+          !props.published && userHasValidSession && postBelongsToUser && (
+            <button onClick={() => publishPost(props.id)}>Publish</button>
+          )
+        }
+        {
+          userHasValidSession && postBelongsToUser && (
+            <button onClick={() => deletePost(props.id)}>Delete</button>
+          )
+        }
       </div>
       <style jsx>{`
         .page {
-          background: white;
+          background: var(--geist-background);
           padding: 2rem;
         }
 
-        .actions {
+       .actions {
           margin-top: 2rem;
         }
 
-        button {
+       button {
           background: #ececec;
           border: 0;
           border-radius: 0.125rem;
           padding: 1rem 2rem;
         }
 
-        button + button {
+       button + button {
           margin-left: 1rem;
         }
       `}</style>
     </Layout>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
